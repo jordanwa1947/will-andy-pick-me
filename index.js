8b9b3c29dbc54cb704f6dc033d85ec177c53bac5
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-04 15:23:11 -0700

    Add favorites column and button that adds questions and responses to the favorites column

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var questionButton = document.getElementById('question-button');
 var eightBall = document.getElementById('eight-ball');
 var questionCont = document.getElementById('question-cont');
 var clearButton = document.getElementById('clear-button');
+var favoriteBttn = document.getElementById('favorite-bttn');
 
 var responses = ['It is certain.',
 "It is decidedly so.",
@@ -25,6 +26,13 @@ var responses = ['It is certain.',
 "Outlook not so good.",
 "Very doubtful."]
 
+function insertQuestionAndResponse(container, question, response) {
+  container.insertAdjacentHTML('afterbegin', `
+    <p id="question-text" class="question">${question}</p>
+    <p id="response-text" class="response">${response}</p>
+  `)
+}
+
 questionField.addEventListener('keyup', (event) => {
   if (event.target.value != '') {
     questionButton.disabled = false;
@@ -36,20 +44,28 @@ questionField.addEventListener('keyup', (event) => {
 questionButton.addEventListener('click', () => {
   var randIndex = Math.floor(Math.random() * responses.length);
   var response = responses[randIndex];
-  var question = questionField.value;
+  var question = `"` + questionField.value + `"`;
   questionField.value = '';
   eightBall.style.display = 'none';
   clearButton.disabled = false;
+  favoriteBttn.disabled = false;
   questionButton.disabled = true;
+  questionCont.innerHTML = '';
   questionCont.style.display = 'flex';
-  questionCont.innerHTML = `
-  <p class='question'>"${question}"</p>
-  <p class='response'>${response}</p>
-  `
-})
+  insertQuestionAndResponse(questionCont, question, response);
+});
 
 clearButton.addEventListener('click', () => {
   clearButton.disabled = true;
+  favoriteBttn.disabled = true;
   eightBall.style.display = 'inline';
   questionCont.style.display = 'none';
+});
+
+favoriteBttn.addEventListener('click', () => {
+  var favorites = document.getElementById('favorites-column');
+  var question = document.getElementById('question-text').innerText;
+  var response = document.getElementById('response-text').innerText;
+  favoriteBttn.disabled = true;
+  insertQuestionAndResponse(favorites, question, response);
 })
