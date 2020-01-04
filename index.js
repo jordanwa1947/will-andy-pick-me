12e57797b7f2b73c9d40cc5a34fef7635a3a67f9
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-04 13:59:32 -0700

    When the user types and submits a question, a random response is chosen and displayed

---
+++ index.js
@@ -0,0 +1,47 @@
+var questionField = document.getElementById('question-field');
+var questionButton = document.getElementById('question-button');
+var eightBall = document.getElementById('eight-ball');
+var questionCont = document.getElementById('question-cont');
+
+var responses = ['It is certain.',
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
+
+questionField.addEventListener('keyup', (event) => {
+  if (event.target.value != '') {
+    questionButton.disabled = false;
+  } else {
+    questionButton.disabled = true;
+  }
+});
+
+questionButton.addEventListener('click', () => {
+  var randIndex = Math.floor(Math.random() * responses.length);
+  var response = responses[randIndex];
+  var question = questionField.value;
+  questionField.value = '';
+  eightBall.style.display = 'none';
+  clearButton.disabled = false;
+  questionCont.style.display = 'flex';
+  questionCont.innerHTML = `
+  <p class='question'>"${question}"</p>
+  <p class='response'>${response}</p>
+  `
+})
